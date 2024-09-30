1adafd68b3220d9335530555dd3e442b745fe621
jayantgit123 <159009863+jayantgit123@users.noreply.github.com>
2024-09-30 15:15:17 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class simple{
     public static void main(String args[]){
-        System.out.println("Hello friends this is Jayant");
+        System.out.println("Hello friends this is Jayant from Capgemini");
     }
 }
